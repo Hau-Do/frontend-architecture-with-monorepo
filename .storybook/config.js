ce8bca9657606326550883fc67364b21ada99639
Peter <haudotien@gmail.com>
2021-09-09 19:59:40 +0700

    setup configuration with Babel, Typescript

---
+++ .storybook/config.js
@@ -0,0 +1,9 @@
+import { configure, addDecorator } from '@storybook/react';
+
+
+const req = require.context('../packages', true, /.story.tsx?$/);
+function loadStories() {
+    req.keys().forEach((filename) => req(filename));
+}
+  
+configure(loadStories, module);
